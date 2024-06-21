a3a498a5960e751e61677544adaa867ea4121ceb
Crowdbotics <team@crowdbotics.com>
2024-06-21 18:40:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "whiskey-app-48516",
+  url: "https://whiskey-app-48516.botics.co",
+  api: "https://whiskey-app-48516.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
